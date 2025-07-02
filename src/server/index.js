594dcf511217cfd5df532f9f8c64f0d13481f8ed
aronurbn <aronurbano111@gmail.com>
2025-07-02 11:23:25 +0800

    Update .jsx

--- src/server/index.js
+++ src/server/index.js
@@ -56,4 +56,4 @@ app.post('/send', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
