5ed6370ee8433e02cf833f10b8dba3e4f2eb52a4
aronurbn <aronurbano111@gmail.com>
2025-07-02 15:28:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -10,24 +10,10 @@ import BackToTop from './components/BackToTop';
 import ScrollProgressBar from './components/ScrollProgressBar';
 
 const App = () => {
-  const [darkMode, setDarkMode] = useState(() => {
-    return localStorage.getItem('theme') === 'dark';
-  });
-
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.documentElement.classList.remove('dark');  // <--- Fix here: remove dark class on light mode
-      localStorage.setItem('theme', 'light'); 
-    }
-  }, [darkMode]);
-
   return (
-    <div className="bg-white dark:bg-black text-black dark:text-white">
+    <div className="bg-black text-white">
       <ScrollProgressBar />
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Navbar />
       <Hero />
       <About />
       <Skills />
