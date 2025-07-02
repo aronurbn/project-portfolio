7030c14ef0bbd08eb570cb6be735a7fd6558fd07
aronurbn <aronurbano111@gmail.com>
2025-07-02 14:45:41 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -29,6 +30,7 @@ const App = () => {
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Hero />
       <About />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
